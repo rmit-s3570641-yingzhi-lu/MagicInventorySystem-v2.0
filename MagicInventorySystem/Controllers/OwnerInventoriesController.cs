9a37a6a1d64a0524ec21be21760fc938ef5e8d68
Shallowpig <s3570641@student.rmit.edu.au>
2018-04-23 20:32:03 +1000

    OwnerInventory 搜索功能 Join the table ownerInventory and product table

--- MagicInventorySystem/Controllers/OwnerInventoriesController.cs
+++ MagicInventorySystem/Controllers/OwnerInventoriesController.cs
@@ -19,10 +19,25 @@ namespace MagicInventorySystem.Controllers
         }
 
         // GET: OwnerInventories
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string productName)
         {
-            var magicInventorySystemContext = _context.OwnerInventory.Include(o => o.Product);
-            return View(await magicInventorySystemContext.ToListAsync());
+            // Eager loading the Product table - join between OwnerInventory and the Product table.
+            var query = _context.OwnerInventory.Include(x => x.Product).Select(x => x);
+
+            if (!string.IsNullOrWhiteSpace(productName))
+            {
+                // Adding a where to the query to filter the data.
+                // Note for the first request productName is null thus the where is not always added.
+                query = query.Where(x => x.Product.Name.Contains(productName));
+
+                // Storing the search into ViewBag to populate the textbox with the same value for convenience.
+                ViewBag.ProductName = productName;
+            }
+            // Adding an order by to the query for the Product name.
+            query = query.OrderBy(x => x.Product.Name);
+
+            // Passing a List<OwnerInventory> model object to the View.
+            return View(await query.ToListAsync());
         }
 
         // GET: OwnerInventories/Details/5
--- MagicInventorySystem/Models/Product.cs
+++ MagicInventorySystem/Models/Product.cs
@@ -1,8 +1,13 @@
-﻿namespace MagicInventorySystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicInventorySystem.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+
+        [Display(Name = "Product Name")]
         public string Name { get; set; }
         public double Value { get; set; }
     }
