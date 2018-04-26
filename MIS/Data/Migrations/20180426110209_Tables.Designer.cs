7925447a9262c5e51ef7e941fbc93104fafc3bd0
Shallowpig <s3570641@student.rmit.edu.au>
2018-04-26 21:05:34 +1000

    Migrate the database Install-Package Microsoft.EntityFrameworkCore.Tools Add-Migration Initial Update-Database

--- MIS/Data/Migrations/20180426110209_Tables.Designer.cs
+++ MIS/Data/Migrations/20180426110209_Tables.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace MIS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180426110209_Tables")]
+    partial class Tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
