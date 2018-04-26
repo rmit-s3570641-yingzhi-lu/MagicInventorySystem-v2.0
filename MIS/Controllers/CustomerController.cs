0398d07864f8d4318fb54b85081c17ac5dab87cb
Shallowpig <s3570641@student.rmit.edu.au>
2018-04-26 23:45:51 +1000

    Store -> store inventory -> list all products sell in this store

--- MIS/Controllers/CustomerController.cs
+++ MIS/Controllers/CustomerController.cs
@@ -14,6 +14,7 @@ namespace MIS.Controllers
     public class CustomerController : Controller
     {
         private readonly ApplicationDbContext _context;
+        public const string SessionKeyStoreID = "_StoreID";
 
         public CustomerController(ApplicationDbContext context)
         {
@@ -26,6 +27,32 @@ namespace MIS.Controllers
             var stores = _context.Store.Select(x => x).OrderBy(x => x.StoreID);
 
             return View(await stores.ToListAsync());
+        }
+
+        public IActionResult List(int? id, string productName)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storeinventory = _context.StoreInventory.Include(x => x.Product).Where(x => x.StoreID == id).Distinct();
+
+            if (!string.IsNullOrWhiteSpace(productName))
+            {
+                storeinventory = storeinventory.Where(x => x.Product.Name.Contains(productName));
+                ViewBag.ProductName = productName;
+            }
+
+            storeinventory = storeinventory.OrderBy(x => x.ProductID);
+
+            if (storeinventory == null)
+            {
+                return NotFound();
+            }
+
+            return View(storeinventory);
+
         }
     }
 }
